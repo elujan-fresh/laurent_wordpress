ebbdda96fe7f63ae364857b7936193b5cd950775
naskafy <naskafy@gmail.com>
2013-10-03 16:28:12 -0500

    plantilla about

---
+++ about.php
@@ -0,0 +1,107 @@
+<?php
+include('header.php');
+?>
+
+     
+       <div id="aboutContent" >
+     
+        <div id="aboutAuthor">
+            <span class="about_title">Biography</span>
+            <div  class="about_info">
+              
+            <p> Named one of the top 50 leaders in the field of New Media by <em>The Hollywood Reporter</em>, director/filmmake/author Laurent Bouzereau is one of the most sought-after talents in the industry.</p>
+            
+            <p>Most recently, Bouzereau wrote, directed and produced the feature-length documentary on producer Richard D. Zanuck entitled DON’T SAY NO UNTIL I FINISH TALKING, executive produced by Steven Spielberg for Amblin Television and TCM.</p>
+
+            <p>In 2011, Laurent Bouzereau directed the feature-length documentary ROMAN POLANSKI: A FILM MEMOIR, which premiered at the 2011 Zurich Film Festival and was selected at the 2012 Cannes Film Festival (Out Of Competition.)</p>
+
+            <p>He is currently the director, writer and producer of the acclaimed television series A NIGHT AT THE MOVIES, documentaries focusing on the history of film genres (Turner Classic Movies/Amblin TV); recent episodes include: THE HORRORS OF STEPHEN KING, HOLLYWOOD GOES TO WASHINGTON and COPS & ROBBERS AND CRIME WRITERS.</p>
+
+
+            <p>Born and raised in France, Bouzereau moved to New York in the early 1980s and worked in several areas of the independent film industry.  During this time he also wrote his first book, THE DE PALMA CUT, a study of the films of director Brian De Palma.  Bouzereau moved to Hollywood, California in 1990 and worked in feature film development for Bette Midler at Disney Studios, while continuing his passion for writing books on cinema.</p>
+
+            <p>In 1994, Bouzereau wrote, directed and produced his first of many documentaries for Steven Spielberg, hence joining the home entertainment revolution.  Bouzereau has directed, written and produced hundreds of documentaries on the “making of” the biggest films in the history of cinema, by some of the most acclaimed directors of all-time, including: Steven Spielberg, James Cameron, Alfred Hitchcock, David Lean, Martin Scorsese, Brian De Palma, William Friedkin, George Lucas, Robert Zemeckis, Warren Beatty, Roman Polanski, Peter Bogdanovich and others. </p>
+
+            <p>As an author, Bouzereau’s credits include HITCHCOCK: PIECE BY PIECE, THE ART OF BOND, STAR WARS: THE ANNOTATED SCREENPLAYS, THE CUTTING ROOM FLOOR, ULTRA VIOLENT MOVIES and others. He most recently contributed to LINCOLN: A CINEMATIC AND HISTORICAL COMPANION.</p>
+
+            <p>Bouzereau currently resides in West Hollywood, CA. </p>
+
+          </div>
+        </div>
+
+        <div id="about_img">
+        </div>
+
+        <div id="aboutAuthorSmall">
+            <span class="about_title">Biography</span>
+
+            <div id="about_imgSmall">
+            </div>
+            <div  class="about_infoSmallRight">
+              
+            <p> Named one of the top 50 leaders in the field of New Media by <em>The Hollywood Reporter</em>, director/filmmake/author Laurent Bouzereau is one of the most sought-after talents in the industry.</p>
+            
+            <p>Most recently, Bouzereau wrote, directed and produced the feature-length documentary on producer Richard D.</p>
+
+          </div>
+
+           <div  class="about_infoSmallDown">
+              
+           
+            <p>Zanuck entitled DON’T SAY NO UNTIL I FINISH TALKING, executive produced by Steven Spielberg for Amblin Television and TCM.</p>
+
+            <p>In 2011, Laurent Bouzereau directed the feature-length documentary ROMAN POLANSKI: A FILM MEMOIR, which premiered at the 2011 Zurich Film Festival and was selected at the 2012 Cannes Film Festival (Out Of Competition.)</p>
+
+            <p>He is currently the director, writer and producer of the acclaimed television series A NIGHT AT THE MOVIES,</p>
+
+            <p>...</p>
+
+            <p>Bouzereau currently resides in West Hollywood, CA. </p>
+
+          </div>
+        </div>
+
+
+        <div id="aboutAwards">
+            <span class="about_titleAwards">Awards & Recognition</span>
+            <div  class="about_infoAwards">
+            <p>
+              <p>DVD CRITICS AWARD
+              <br>
+                Best Blu-Ray Catalogue Title: 
+                <br><em>Close Encounters Of The Third Kind</em> (2008)
+              </p>
+              <p><em>THE HOLLYWOOD REPORTER'S</em> “DIGITAL 50”
+                <br>
+                Listed as a leader in the world of Digital Media (2006 & 2007)
+              </p>
+
+              <p>DVD EXCLUSIVE AWARDS
+                <br>
+                Pioneer Award (2004)
+              </p>
+                        
+              <p>FIMO-Festival International du Making Of (France)
+                <br>
+                Achievement Award (2003)
+              </p>
+              <p>DVD EXCLUSIVE AWARDS
+                <br/>
+                Best Documentary: <em>The Birds</em> DVD (2000) 
+              </p>
+
+            </div>
+        </div>
+        
+      </div>
+
+
+
+
+  
+       
+
+
+<?php 
+include('footer.php');
+?>
