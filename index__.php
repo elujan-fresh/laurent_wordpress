0d7dd7e2c9010de2bc0a900194ac695293fabd20
Eduardo Lujan <eduardo.lujan.p@gmail.com>
2013-10-03 13:50:56 -0500

    Wordpress template struct

--- index__.php
+++ index__.php
@@ -1,6 +1,8 @@
-<?php get_header(); ?>
-	
- <div id="slideshow" >
+<?php
+include('header.php');
+?>
+
+     <div id="slideshow" >
         <div id="slideshow_left" class="prev">
         </div>
 
@@ -64,5 +66,12 @@
               </p>
         </div>
       </div>
-      
-<?php get_footer(); ?>
+
+
+  
+       
+
+
+<?php 
+include('footer.php');
+?>
