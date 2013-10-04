7a3a77ce6138f796b1f7b16f80416b68d3a65676
naskafy <naskafy@gmail.com>
2013-10-04 11:44:37 -0500

    se agrega nuevamente documentaries

---
+++ documentaries.php
@@ -0,0 +1,38 @@
+<?php
+include('header.php');
+?>
+
+<div id="documentaries">
+        <div id="documentaries_content">
+          <p class="documentaries_title">Roman Polanski: A Film Memoir </p>
+          <p class="documentaries_text">
+            The documentary ROMAN POLANSKI: A FILM MEMOIR tells the extraordinary story of Roman Polanski’s life, beginning with his childhood in the Cracow ghetto, his first films in Poland, the move to Paris, his career in Europe and America, crowned with an Oscar for THE PIANIST, the tragedy of the murder of his pregnant wife Sharon Tate in Los Angeles, the controversy surrounding his arrest in 1977, through to his work and life today in France with his wife Emmanuelle Seigner
+          </p>
+         <br>
+          <div id="line"></div>
+         
+          <p class="documentaries_quote">
+            ”…people see a human being, thoughtful, eloquent and emotional as he reflects on what, by any accounts, has been an extraordinary life.”   <span class="text_small">  -The Hollywood Reporter</span>
+          </p>
+          <div id="line"></div>
+          
+        <br>
+          <p class="documentaries_text">
+          The conversations were recorded during Roman Polanski‘s stay in his home in Gstaad where he was under house arrest for several months after he was apprehended on his way to the Zurich Film Festival in 2009. The conversations are illustrated with excerpts from Polanski’s films, news footage, press clippings, private and exclusive photos, and documents chronicling the filmmaker’s extraordinary life.   The film premiered at the Zurich Film Festival in 2011..
+          </p>
+          <div id="documentaries_paginator">
+                        
+              <ul>
+                <li class="active"><span class="paginator_active">1</span></li>
+                <li><span class="paginator"><a href="new.php" style="text-decoration:none;">2</a></span></li>
+               
+              </ul>
+            
+          </div>
+        </div>
+        
+      </div>
+
+<?php 
+include('footer.php');
+?>
