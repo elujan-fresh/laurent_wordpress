94640f669a59d841749fde4437373a633002dd9f
Eduardo Lujan <eduardo.lujan.p@gmail.com>
2013-10-08 19:02:39 -0500

    changes:

--- js/main.js
+++ js/main.js
@@ -13,46 +13,24 @@ $(window).load(function (){
 
 
 
-	$('#slideshowImgStyle').carouFredSel({
-		prev: '.prev',
-		next: '.next',	
-	});
-	
-	
-	$('#behindTheScenes_slideshowImgStyle').carouFredSel({
+	$('#slideshowImgStyle_1024').carouFredSel({
 		prev: '.prev',
 		next: '.next',	
 	});
 
+	$('#slideshowImgStyle_1600').carouFredSel({
+		prev: '.prev',
+		next: '.next',	
+	});
+	
+	
+	
+
+	
 
 	$("#slideshowImgStyle a").fancybox();
 
-	x = 0;
-	$(window).resize(function(){
-		x = $(window).width();
-		console.log(x)
 
-		if(x >=480 && x<768)
-		{
-			x = 480;
-		}
-
-		if(x >=768 && x<1024)
-		{
-			x = 768;
-		}
-
-		if(x >=1024 && x<1600)
-		{
-			x = 1024;
-		}
-
-		if(x >=1600)
-		{
-			x = 1600;
-		}	
-		console.log(x)	
-	})
 	
 
 
